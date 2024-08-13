6db4f44841dcebdd025d566bd5e77df0390e03d1
Ranjit Rupnawar <162142856+ranjitrupnawar@users.noreply.github.com>
2024-08-13 11:36:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 30000;
 
 // Define a simple API endpoint
 app.get('/', (req, res) => {
-  res.send('Arre bhava kute  ');
+  res.send('Arre bhava kute ahe ');
 });
 
 // Start the server
